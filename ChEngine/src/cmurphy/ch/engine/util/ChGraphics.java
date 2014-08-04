ef147f6a440d88eafb8cdebd1569fca01ba5ecd0
Experimental1212 <cmhurripsh@gmail.com>
2014-08-03 22:36:59 -0400

    ChEngine.graphics() optimization and bug fix

--- ChEngine/src/cmurphy/ch/engine/util/ChGraphics.java
+++ ChEngine/src/cmurphy/ch/engine/util/ChGraphics.java
@@ -28,7 +28,18 @@ public class ChGraphics {
     }
     
     public void gradient(java.awt.Point p1, java.awt.Point p2, int col1, int col2) {
-
+        if(p1.x > p2.x) {
+            java.awt.Point tempPoint = p1;
+            p1 = p2;
+            p2 = tempPoint;
+        }
+        
+        if(col1 > col2) {
+            int tempCol = col1;
+            col1 = col2;
+            col2 = tempCol;
+        }
+        
         double phi = Math.atan((p1.y - p2.y) / (double)(p2.x - p1.x));
         double cosPhi = Math.cos(phi);
         double sinPhi = Math.sin(phi);
@@ -38,15 +49,29 @@ public class ChGraphics {
         double gd = (col2 / 0x100 % 0x100 - col1 / 0x100 % 0x100) / (double)numCols;
         double bd = (col2 % 0x100 - col1 % 0x100) / (double)numCols;
         
+        int x = 0, y = 0;
+        double xx;
+        int rt = 0, gt = 0, bt = 0;
         for (int i = 0; i < pixels.length; i++) {
-            int x = i % width - p1.x;
-            int y = i / width - p1.y;
-            double xx = x * cosPhi - y * sinPhi;
-            if(xx < 0) xx = 0;
-            if(xx > numCols) xx = numCols;
-            int rt = col1 + (int)(rd * xx);
-            int gt = col1 + (int)(gd * xx);
-            int bt = col1 + (int)(bd * xx);
+            x = i % width - p1.x;
+            y = i / width - p1.y;
+            xx = x * cosPhi - y * sinPhi;
+            
+            if(xx < 0) {
+                rt = col1;
+                gt = col1;
+                bt = col1;
+            }
+            else if(xx > numCols) {
+                rt = col1 + (int)(rd * numCols);
+                gt = col1 + (int)(gd * numCols);
+                bt = col1 + (int)(bd * numCols);
+            }
+            else {
+                rt = col1 + (int)(rd * xx);
+                gt = col1 + (int)(gd * xx);
+                bt = col1 + (int)(bd * xx);
+            }
             
             pixels[i] = rt * 0x10000 + gt * 0x100 + bt;
         }
